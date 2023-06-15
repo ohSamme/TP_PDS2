644e1ccaffd68c3a920a9e2fe57b12783142a30c
ohSamme <samuelvladimirfc@gmail.com>
2023-06-15 18:09:26 -0300

    Core Normalização

---
+++ normalizacao.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+using namespace std;
+
+class Normalizacao{
+public:
+    void minusculo(string& palavra){
+        //////
+        //////
+
+
+
+
+    }
+
+    void remover_caracter_especial(string& palavra){
+        /////
+        /////
+    }
+};
