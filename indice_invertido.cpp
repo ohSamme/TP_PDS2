b796847568771fe8a181b3d725b3e6e09625e465
ohSamme <samuelvladimirfc@gmail.com>
2023-06-15 18:19:55 -0300

    Core Indice Invertido

---
+++ indice_invertido.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+class InvertedIndex{
+    public:
+        void indice(const std::string& directory){
+            /////
+            /////
+        }
+
+        void imprime() const{
+            ////
+            ////
+        }
+    
+private:
+   //map<string, map<string, int>> index;
+};
