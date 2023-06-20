a3fa87afbbbac7850cec970a5435303083ae5fb5
ohSamme <samuelvladimirfc@gmail.com>
2023-06-20 18:26:39 -0300

    Implementando normalizacao.h

---
+++ normalizacao.h
@@ -0,0 +1,12 @@
+#ifndef NORMALIZACAO_H
+#define NORMALIZACAO_H
+
+#include <string>
+
+class Normalizacao {
+public:
+    void minusculo(std::string& palavra);
+    void remover_caracter_especial(std::string& palavra);
+};
+
+#endif  // NORMALIZACAO_H
