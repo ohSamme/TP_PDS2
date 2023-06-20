70c55bc3d9982e18c2fdcd7c5ee2635a022f5f90
Daniel Neves <dnpinheiro@hotmail.com>
2023-06-20 20:33:48 -0300

    Implementacao_normalizacao

--- normalizacao.cpp
+++ normalizacao.cpp
@@ -1,20 +1,28 @@
 #include <string>
+#include "normalizacao.h"
 
 using namespace std;
 
-class Normalizacao{
-public:
-    void minusculo(string& palavra){
-        //////
-        //////
-
-
-
-
+void Normalizacao::minusculo(string& palavra)
+{
+    for (int i = 0; i < palavra.size(); i++)
+    {
+        if (palavra[i] >= 65 && palavra[i] <= 90)
+        {
+            palavra[i] = palavra[i] + 32;
+        }
     }
+}
 
-    void remover_caracter_especial(string& palavra){
-        /////
-        /////
+void Normalizacao::remover_caracter_especial(string& palavra)
+{
+    string aux;
+    for (int i = 0; i < palavra.size(); i++)
+    {
+        if ((palavra[i] >= 65 && palavra[i] <= 90) || (palavra[i] >= 97 && palavra[i] <= 122))
+        {
+            aux += palavra[i];
+        }
     }
-};
+    palavra = aux;
+}
