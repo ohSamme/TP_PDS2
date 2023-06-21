65ca28bb94ccb44a0d49d3c7ae83e2a7946f7626
ohSamme <samuelvladimirfc@gmail.com>
2023-06-20 23:42:21 -0300

    Indice sem tratamento de erros

--- indice_invertido.cpp
+++ indice_invertido.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <sstream>
+#include <fstream>
 #include <map>
+#include "normalizacao.h"
+#include "indice_invertido.h"
 
-class InvertedIndex{
-    public:
-        void indice(const std::string& directory){
-            /////
-            /////
-        }
+using namespace std;
 
-        void imprime() const{
-            ////
-            ////
+void InvertedIndex::indice(const string &directory)
+{
+    Normalizacao normalizacao;
+    for (const auto &entry : __fs::filesystem::directory_iterator(directory))
+    {
+        try
+        {
+            ifstream infile(entry.path());
+            if (infile.good())
+            {
+                string palavra;
+                while (infile >> palavra)
+                {
+                    normalizacao.remover_caracter_especial(palavra);
+                    if (!palavra.empty())
+                    {
+                        normalizacao.minusculo(palavra);
+                        index[palavra][entry.path().filename()]++;
+                    }
+                }
+                infile.close();
+            }
+            else{}
         }
-    
-private:
-   //map<string, map<string, int>> index;
-};
+        catch (...){}
+    }
+}
