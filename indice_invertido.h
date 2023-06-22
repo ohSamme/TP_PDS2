2ae3f711c231b06d92fbecedc7a96abc77c21ebf
ohSamme <samuelvladimirfc@gmail.com>
2023-06-22 17:09:42 -0300

    Implementacao todas_presentes .h

--- indice_invertido.h
+++ indice_invertido.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
 
 using namespace std;
 
@@ -12,6 +13,8 @@ public:
     void indice(const string& directory);
     
     vector<string> vetor_de_palavras(string frase);
+
+    set<string> todas_presentes(vector<string> vetor_palavras);
 
 private:
     map<string, map<string, int>> index;
