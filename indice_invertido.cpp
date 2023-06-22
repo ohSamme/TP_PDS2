2967f1c94c9549abfac5654d9f9f4e35f0d6560e
ohSamme <samuelvladimirfc@gmail.com>
2023-06-22 16:57:16 -0300

    Tratamento de erros e ajuste indice

--- indice_invertido.cpp
+++ indice_invertido.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
-#include <string>
-#include <filesystem>
-#include <sstream>
 #include <fstream>
 #include <map>
+#include <vector>
+#include <algorithm>
+#include <filesystem>
+#include <sstream>
+#include <set>
 #include "normalizacao.h"
 #include "indice_invertido.h"
 
 using namespace std;
 
-void InvertedIndex::indice(const string &directory)
-{
+void InvertedIndex::indice(const string& directory){
     Normalizacao normalizacao;
-    for (const auto &entry : __fs::filesystem::directory_iterator(directory))
-    {
-        try
-        {
+    for (const auto& entry : fs::filesystem::directory_iterator(directory)){
+        try {
             ifstream infile(entry.path());
-            if (infile.good())
-            {
+            if (infile.good()) {
                 string palavra;
-                while (infile >> palavra)
-                {
+                while (infile >> palavra) {
                     normalizacao.remover_caracter_especial(palavra);
-                    if (!palavra.empty())
-                    {
+                    if (!palavra.empty()) {
                         normalizacao.minusculo(palavra);
                         index[palavra][entry.path().filename()]++;
                     }
                 }
                 infile.close();
             }
-            else{}
+            else {
+                throw runtime_error("Erro ao abrir o arquivo: " + entry.path().string());
+            }
         }
-        catch (...){}
+        catch (...) {
+            printf("Erro desconhecido!");
+        }
     }
 }
